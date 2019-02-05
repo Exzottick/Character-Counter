b2e812699d2060e8e9e774da143cdee93c75ac11
Dylan Sigler <jsigler47@gmail.com>
2019-02-04 20:27:37 -0700

    Add files via upload

---
+++ charCounter.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main(){
+
+	int charCount[26] = {0}, i;
+	char c;
+
+	while(c = getchar()){
+		if(c == EOF)
+			break;
+		else{
+			if(isalpha(c)){
+				charCount[tolower(c) - 'a']++;
+				//std::cout << tolower(c) - 'a' << std::endl;
+			}
+		}
+
+	}
+	c = 'A';
+	for(i = 0; i < 26; i++){
+
+		std::cout << c++ << " : " << charCount[i] << std::endl;
+	}
+
+}
